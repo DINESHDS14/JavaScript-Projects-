407e1c2d26b180cf74239707378c26bcb25ab842
DINESH <113289557+DINESHDS14@users.noreply.github.com>
2023-11-06 12:24:43 +0530

    Add files via upload

---
+++ Counter app/script.js
@@ -0,0 +1,29 @@
+const countDisplay = document.getElementById("count")
+const incrementButton = document.getElementById("increment")
+const decrementButton = document.getElementById("decrement")
+const resetButton = document.getElementById("reset")
+
+let count = 0
+
+function updateCount(){
+    countDisplay.textContent = count
+}
+
+incrementButton.addEventListener("click", ()=>{
+    count++
+    updateCount()
+})
+
+decrementButton.addEventListener("click",()=>{
+    if(count > 0){
+        count--
+        updateCount()
+    }
+})
+
+resetButton.addEventListener("click",()=>{
+    count = 0
+    updateCount()
+})
+
+updateCount()
